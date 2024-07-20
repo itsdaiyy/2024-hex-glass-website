d8e8db220fcb6636a8350263714e6f577829e2e6
Ollie D <behoya11@gmail.com>
2024-07-20 15:14:53 +0800

    修改 repository 名稱

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: "/2024-hex-w2-vite/",
+  base: "/2024-hex-w3-glass-website/",
   plugins: [
     liveReload(["./layout/**/*.ejs", "./pages/**/*.ejs", "./pages/**/*.html"]),
     ViteEjsPlugin(),
